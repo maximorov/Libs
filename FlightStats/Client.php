1865ef6bab1c6aae4cbd3f35bef7b1f06acdc17b
Maximorov <maximorov@gmail.com>
2015-08-11 16:45:24 +0300

    Add cacher

--- FlightStats/Client.php
+++ FlightStats/Client.php
@@ -5,8 +5,13 @@
  */
 class FlightStats_Client
 {
+    const CACHE_KEY = 'fl-st:';
+    const CACHE_TTL = 86400;
+
     public static function getFlights($iataCode, $year, $month, $day, $hour)
     {
+        $iataCode = strtoupper($iataCode);
+
         $url = "flex/schedules/rest/v1/json/from/$iataCode/departing/$year/$month/$day/$hour?" . self::getKeyQuery();
 
         return self::getResult($url);
@@ -14,12 +19,23 @@ class FlightStats_Client
 
     private static function getResult($url)
     {
+        $cacher = Mage::app()->getCache();
+        $cache_key = self::CACHE_KEY . md5($url);
+        $result = $cacher->load($cache_key);
+        if ($result) {
+            return unserialize($result);
+        }
+
         $json_result = file_get_contents(FlightStats_Conf::get('url') . $url);
         if ($http_response_header !== null &&
             isset($http_response_header[0]) &&
             strpos($http_response_header[0], '200') !== false)
         {
-            return new FlightStats_Result($json_result);
+            $result = new FlightStats_Result($json_result);
+
+            $cacher->save(serialize($result), $cache_key, ['flight-stats'], self::CACHE_TTL);
+
+            return $result;
         }
 
         throw new Exception('Can\'t get response from flights stats server');
--- FlightStats/README.md
+++ FlightStats/README.md
@@ -1,1 +1,8 @@
-API for FlightStats.com v 0.01
+API for FlightStats.com v 0.01 FOR Magento v 1.9.2
+
+Example:
+$available_airports = FlightStats_Conf::get('available_airports');
+
+$result = FlightStats_Client::getFlights(current(array_keys($available_airports)), 2015, 8, 11, 12);
+
+print_r($result->getScheduledFlights());
